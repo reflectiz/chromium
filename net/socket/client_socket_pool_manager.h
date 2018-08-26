dc6606bbe94f24cc1f619d8523e0bb53b23a4854
U-AzureAD\GregoryRosenbaum <user.name>
2018-08-26 19:20:09 +0300

    Modified kDefaultMaxSocketsPerProxyServer

--- net/socket/client_socket_pool_manager.h
+++ net/socket/client_socket_pool_manager.h
@@ -43,7 +43,7 @@ struct SSLConfig;
 
 // This should rather be a simple constant but Windows shared libs doesn't
 // really offer much flexiblity in exporting contants.
-enum DefaultMaxValues { kDefaultMaxSocketsPerProxyServer = 32 };
+enum DefaultMaxValues { kDefaultMaxSocketsPerProxyServer = 256 };
 
 class NET_EXPORT_PRIVATE ClientSocketPoolManager {
  public:
